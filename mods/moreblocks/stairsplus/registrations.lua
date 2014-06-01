9e69c0b5b82ebe0471fc83b2ef9e7a61d79f7319
Craig <craig.davison3@gmail.com>
2014-06-01 10:14:35 +0100

    Fix aspects of crafting with stone/wood
    Related to #1

--- mods/moreblocks/stairsplus/registrations.lua
+++ mods/moreblocks/stairsplus/registrations.lua
@@ -12,7 +12,7 @@ local default_nodes = {
 	"bronzeblock",
 	"diamondblock",
 	"desert_stone",
-	-- "desert_cobble", Does not work in minetest_game.
+--	"desert_cobble", -- Does not work in minetest_game.
 	"glass",
 	"tree",
 	"wood",
@@ -29,7 +29,12 @@ for _, name in pairs(default_nodes) do
 	local nodename = "default:"..name
 	local ndef = minetest.registered_nodes[nodename]
 	local groups = {}
-	for k, v in pairs(ndef.groups) do groups[k] = v end
+	for k, v in pairs(ndef.groups)
+		-- Ignore wood and stone groups to not make them usable in crafting.
+		do if k ~= "wood" and k ~= "stone" then
+			groups[k] = v
+		end
+	end
 	local drop
 	if type(ndef.drop) == "string" then
 		drop = ndef.drop:sub(9)
