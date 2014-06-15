4d5e0bac897eee109e6799f15d52d413283e67a3
Craig <craig.davison3@gmail.com>
2014-06-15 10:41:43 +0100

    Update 3d_armor modpack

--- mods/shields/init.lua
+++ mods/shields/init.lua
@@ -1,3 +1,5 @@
+local use_moreores = minetest.get_modpath("moreores")
+
 -- Regisiter Shields
 
 minetest.register_tool("shields:shield_wood", {
@@ -28,13 +30,33 @@ minetest.register_tool("shields:shield_diamond", {
 	wear = 0,
 })
 
+minetest.register_tool("shields:shield_gold", {
+	description = "Gold Shield",
+	inventory_image = "shields_inv_shield_gold.png",
+	groups = {armor_shield=10, armor_heal=6, armor_use=250},
+	wear = 0,
+})
+
+if use_moreores then
+	minetest.register_tool("shields:shield_mithril", {
+		description = "Mithril Shield",
+		inventory_image = "shields_inv_shield_mithril.png",
+		groups = {armor_shield=15, armor_heal=12, armor_use=50},
+		wear = 0,
+	})
+end
 
 local craft_ingreds = {
 	wood = "default:wood",
 	steel = "default:steel_ingot",
 	bronze = "default:bronze_ingot",
 	diamond = "default:diamond",
+	gold = "default:gold_ingot",
 }	
+
+if has_moreores then
+	craft_ingreds.mithril = "moreores:mithril_ingot"
+end
 
 for k, v in pairs(craft_ingreds) do
 	minetest.register_craft({
@@ -49,13 +71,6 @@ end
 
 minetest.after(0, function()
 	table.insert(armor.elements, "shield")
-	armor.formspec = "size[8,8.5]button[0,0;2,0.5;main;Back]"
-		.."list[current_player;main;0,4.5;8,4;]"
-		.."list[detached:player_name_armor;armor_head;3,0;1,1;]"
-		.."list[detached:player_name_armor;armor_torso;3,1;1,1;]"
-		.."list[detached:player_name_armor;armor_legs;3,2;1,1;]"
-		.."list[detached:player_name_armor;armor_feet;3,3;1,1;]"
-		.."list[detached:player_name_armor;armor_shield;4,1;1,1;]"
-end)		
+end)
 
-
+ 
