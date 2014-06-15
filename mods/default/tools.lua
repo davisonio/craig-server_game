f05597887faf20af6d793da1cb6427ed63b11a41
Craig <craig.davison3@gmail.com>
2014-06-15 19:15:42 +0100

    Longer range

--- mods/default/tools.lua
+++ mods/default/tools.lua
@@ -5,6 +5,7 @@ minetest.register_item(":", {
 	type = "none",
 	wield_image = "wieldhand.png",
 	wield_scale = {x=1,y=1,z=2.5},
+	range = 8,
 	tool_capabilities = {
 		full_punch_interval = 0.9,
 		max_drop_level = 0,
