55aa0112212f95149152d39ade6d8a590d9be938
CraigyDavi <craig.davison3@gmail.com>
2014-08-17 11:41:40 +0100

    Rename _server_misc_ to _misc

---
+++ mods/_misc/init.lua
@@ -0,0 +1,27 @@
+---------------------
+-- Server Misc Mod --
+---------------------
+
+--News
+dofile(minetest.get_modpath("_misc").."/news.lua")
+
+--Rules
+dofile(minetest.get_modpath("_misc").."/rules.lua")
+
+-- Aliases
+dofile(minetest.get_modpath("_misc").."/aliases.lua")
+
+-- Chatlog
+dofile(minetest.get_modpath("_misc").."/chatlog.lua")
+
+-- Username filter
+dofile(minetest.get_modpath("_misc").."/username_filter.lua")
+
+-- Ban
+dofile(minetest.get_modpath("_misc").."/ban.lua")
+
+-- Chat Offline
+dofile(minetest.get_modpath("_misc").."/chat_offline.lua")
+
+-- No Interact Messages
+dofile(minetest.get_modpath("_misc").."/nointeract_messages.lua")
