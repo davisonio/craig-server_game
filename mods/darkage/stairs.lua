50aa9e957b6d85755fddb077a5d31d148553dc9f
CraigyDavi <craig.davison3@gmail.com>
2014-09-22 21:27:52 +0100

    Use my fork of the darkage mod, w/ classic textures!
    <3

--- mods/darkage/stairs.lua
+++ mods/darkage/stairs.lua
@@ -1,155 +1,169 @@
-darkage = {}
-
-function darkage.register_stairs(modname, item, groups, images, description)
-  local recipeitem = modname..":"..item
-  local itemname = modname..":stair_"..item
-	minetest.register_node(itemname, {
-		description = description.." stair",
-		drawtype = "nodebox",
-		tiles = images,
+	stairsplus:register_all("darkage", "basalt", "darkage:basalt", {
+		description = "Basalt",
+		tiles = {"darkage_basalt.png"},
+		groups = {cracky=3},
+		sounds = default.node_sound_stone_defaults(),
+		sunlight_propagates = true,
+	})
+	
+	stairsplus:register_all("darkage", "marble", "darkage:marble", {
+		description = "Marble",
+		tiles = {"darkage_marble.png"},
+		groups = {cracky=3},
+		sounds = default.node_sound_stone_defaults(),
+		sunlight_propagates = true,
+	})
+	
+	stairsplus:register_all("darkage", "serpentine", "darkage:serpentine", {
+		description = "Serpentine",
+		tiles = {"darkage_serpentine.png"},
+		groups = {cracky=3},
+		sounds = default.node_sound_stone_defaults(),
+		sunlight_propagates = true,
+	})
+	
+	stairsplus:register_all("darkage", "ors", "darkage:ors", {
+		description = "Old Red Sandstone",
+		tiles = {"darkage_ors.png"},
+		groups = {crumbly=2,cracky=2},
+		sounds = default.node_sound_stone_defaults(),
+		sunlight_propagates = true,
+	})
+	
+	stairsplus:register_all("darkage", "schist", "darkage:schist", {
+		description = "Schist",
+		tiles = {"darkage_schist.png"},
+		groups = {cracky=3},
+		sounds = default.node_sound_stone_defaults(),
+		sunlight_propagates = true,
+	})
+	
+	stairsplus:register_all("darkage", "slate", "darkage:slate", {
+		description = "Slate",
+		tiles = {"darkage_slate.png"},
+		groups = {cracky=2},
+		sounds = default.node_sound_stone_defaults(),
+		sunlight_propagates = true,
+	})
+	
+	stairsplus:register_all("darkage", "gneiss", "darkage:gneiss", {
+		description = "Gneiss",
+		tiles = {"darkage_gneiss.png"},
+		groups = {cracky=3},
+		sounds = default.node_sound_stone_defaults(),
+		sunlight_propagates = true,
+	})
+	
+	stairsplus:register_all("darkage", "chalk", "darkage:chalk", {
+		description = "Chalk",
+		tiles = {"darkage_chalk.png"},
+		groups = {crumbly=2,cracky=2},
+		sounds = default.node_sound_stone_defaults(),
+		sunlight_propagates = true,
+	})
+	
+	stairsplus:register_all("darkage", "slate_cobble", "darkage:slate_cobble", {
+		description = "Slate Cobble",
+		tiles = {"darkage_slate_brick.png"},
+		groups = {cracky=2},
+		sounds = default.node_sound_stone_defaults(),
+		sunlight_propagates = true,
+	})
+	
+	stairsplus:register_all("darkage", "ors_cobble", "darkage:ors_cobble", {
+		description = "Old Red Sandstone Cobble",
+		tiles = {"darkage_ors_brick.png"},
+		groups = {crumbly=2,cracky=2},
+		sounds = default.node_sound_stone_defaults(),
+		sunlight_propagates = true,
+	})
+	
+	stairsplus:register_all("darkage", "gneiss_cobble", "darkage:gneiss_cobble", {
+		description = "Gneiss Cobble",
+		tiles = {"darkage_gneiss_brick.png"},
+		groups = {cracky=3},
+		sounds = default.node_sound_stone_defaults(),
+		sunlight_propagates = true,
+	})
+	
+	stairsplus:register_all("darkage", "basalt_cobble", "darkage:basalt_cobble", {
+		description = "Basalt Cobble",
+		tiles = {"darkage_basalt_brick.png"},
+		groups = {cracky=3},
+		sounds = default.node_sound_stone_defaults(),
+		sunlight_propagates = true,
+	})
+	
+	stairsplus:register_all("darkage", "straw", "darkage:straw", {
+		description = "Straw",
+		tiles = {"darkage_straw.png"},
+		groups = {snappy=3, flammable=2},
+		sounds = default.node_sound_stone_defaults(),
+		sunlight_propagates = true,
+	})
+	
+	stairsplus:register_all("darkage", "straw_bale", "darkage:straw_bale", {
+		description = "Straw Bale",
+		tiles = {"darkage_straw_bale.png"},
+		groups = {snappy=2, flammable=2},
+		sounds = default.node_sound_stone_defaults(),
+		sunlight_propagates = true,
+	})
+	
+	stairsplus:register_all("darkage", "slate_tile", "darkage:slate_tile", {
+		description = "Slate Tile",
+		tiles = {"darkage_slate_tile.png"},
+		groups = {cracky=2},
+		sounds = default.node_sound_stone_defaults(),
+		sunlight_propagates = true,
+	})
+	
+	stairsplus:register_all("darkage", "marble_tile", "darkage:marble_tile", {
+		description = "Marble Tile",
+		tiles = {"darkage_marble_tile.png"},
+		groups = {cracky=2},
+		sounds = default.node_sound_stone_defaults(),
+		sunlight_propagates = true,
+	})
+	
+	stairsplus:register_all("darkage", "stone_brick", "darkage:stone_brick", {
+		description = "Stone Brick",
+		tiles = {"darkage_stone_brick.png"},
+		groups = {cracky=3},
+		sounds = default.node_sound_stone_defaults(),
+		sunlight_propagates = true,
+	})
+	
+	stairsplus:register_all("darkage", "reinforced_chalk", "darkage:reinforced_chalk", {
+		description = "Reinforced Chalk",
+		tiles = {"darkage_chalk.png^darkage_reinforce.png"},
+		groups = {cracky=3},
+		sounds = default.node_sound_stone_defaults(),
+		sunlight_propagates = true,
+	})
+	
+	stairsplus:register_all("darkage", "glass", "darkage:glass", {
+		description = "Medieval Glass",
+		tiles = {"darkage_glass.png"},
+		groups = {snappy=2,cracky=3,oddly_breakable_by_hand=3},
+		sounds = default.node_sound_stone_defaults(),
+		sunlight_propagates = true,
+	})
+	
+	stairsplus:register_all("darkage", "glow_glass", "darkage:glow_glass", {
+		description = "Medieval Glow Glass",
+		tiles = {"darkage_glass.png"},
 		paramtype = "light",
-		paramtype2 = "facedir",
-		is_ground_content = true,
-		groups = groups,
-		node_box = {
-			type = "fixed",
-			fixed = {
-				{-0.5, -0.5, -0.5, 0.5, 0, 0.5},
-				{-0.5, 0, 0, 0.5, 0.5, 0.5},
-			},
-		},
+		light_source = 18,
+		groups = {snappy=2,cracky=3,oddly_breakable_by_hand=3},
+		sounds = default.node_sound_stone_defaults(),
+		sunlight_propagates = true,
 	})
-
-	minetest.register_craft({
-		output = itemname .. ' 4',
-		recipe = {
-			{recipeitem, "", ""},
-			{recipeitem, recipeitem, ""},
-			{recipeitem, recipeitem, recipeitem},
-		},
+	
+	stairsplus:register_all("darkage", "reinforced_wood", "darkage:reinforced_wood", {
+		description = "Reinforced Wood",
+		tiles = {"default_wood.png^darkage_reinforce.png"},
+		groups = {snappy=2,choppy=3,oddly_breakable_by_hand=3,flammable=3},
+		sounds = default.node_sound_stone_defaults(),
+		sunlight_propagates = true,
 	})
-
-	-- Flipped recipe for the silly minecrafters
-	minetest.register_craft({
-		output = itemname .. ' 4',
-		recipe = {
-			{"", "", recipeitem},
-			{"", recipeitem, recipeitem},
-			{recipeitem, recipeitem, recipeitem},
-		},
-	})
-
-  itemname=modname..":slab_" .. item
-
-	minetest.register_node(itemname, {
-		description = description.." slab",
-		drawtype = "nodebox",
-		tiles = images,
-		paramtype = "light",
-		is_ground_content = true,
-		groups = groups,
-		node_box = {
-			type = "fixed",
-			fixed = {-0.5, -0.5, -0.5, 0.5, 0, 0.5},
-		},
-		selection_box = {
-			type = "fixed",
-			fixed = {-0.5, -0.5, -0.5, 0.5, 0, 0.5},
-		},
-		on_place = function(itemstack, placer, pointed_thing)
-			if pointed_thing.type ~= "node" then
-				return itemstack
-			end
-
-			-- If it's being placed on an another similar one, replace it with
-			-- a full block
-			local slabpos = nil
-			local slabnode = nil
-			local p0 = pointed_thing.under
-			local p1 = pointed_thing.above
-			local n0 = minetest.env:get_node(p0)
-			local n1 = minetest.env:get_node(p1)
-			if n0.name == itemname then
-				slabpos = p0
-				slabnode = n0
-			elseif n1.name == itemname then
-				slabpos = p1
-				slabnode = n1
-			end
-			if slabpos then
-				-- Remove the slab at slabpos
-				minetest.env:remove_node(slabpos)
-				-- Make a fake stack of a single item and try to place it
-				local fakestack = ItemStack(recipeitem)
-				pointed_thing.above = slabpos
-				fakestack = minetest.item_place(fakestack, placer, pointed_thing)
-				-- If the item was taken from the fake stack, decrement original
-				if not fakestack or fakestack:is_empty() then
-					itemstack:take_item(1)
-				-- Else put old node back
-				else
-					minetest.env:set_node(slabpos, slabnode)
-				end
-				return itemstack
-			end
-			
-			-- Otherwise place regularly
-			return minetest.item_place(itemstack, placer, pointed_thing)
-		end,
-	})
-
-	minetest.register_craft({
-		output = itemname .. ' 3',
-		recipe = {
-			{recipeitem, recipeitem, recipeitem},
-		},
-	})
-end
-
-darkage.register_stairs("darkage","basalt_cobble",
-	{cracky=3, not_in_creative_inventory=1, not_in_craft_guide=1},
-	{"darkage_basalt_cobble.png"},
-	"Basalt Cobble"
-)
-
-darkage.register_stairs("darkage","slate_tile",
-	{cracky=3, not_in_creative_inventory=1, not_in_craft_guide=1},
-	{"darkage_slate_tile.png"},
-	"Slate Tile"
-)
-
-darkage.register_stairs("darkage","straw",
-	 {snappy=3, flammable=2, not_in_creative_inventory=1, not_in_craft_guide=1},
-	{"darkage_straw.png"},
-	"Straw"
-)
-
-darkage.register_stairs("darkage","stone_brick",
-	{cracky=3, not_in_creative_inventory=1, not_in_craft_guide=1},
-	{"darkage_stone_brick.png"},
-	"Stone Brick"
-)
-
-darkage.register_stairs("darkage","marble",
-	{cracky=3, not_in_creative_inventory=1, not_in_craft_guide=1},
-	{"darkage_marble.png"},
-	"Marble"
-)
-
-darkage.register_stairs("darkage","marble_tile",
-	{cracky=3, not_in_creative_inventory=1, not_in_craft_guide=1},
-	{"darkage_marble_tile.png"},
-	"Marble Tile"
-)
-
-darkage.register_stairs("darkage","ors_cobble",
-	{cracky=3, not_in_creative_inventory=1, not_in_craft_guide=1},
-	{"darkage_ors_cobble.png"},
-	"Old Red Sandstone"
-)
-
-darkage.register_stairs("darkage","serpentine",
-	{cracky=3, not_in_creative_inventory=1, not_in_craft_guide=1},
-	{"darkage_serpentine.png"},
-	"Serpentine"
-)
