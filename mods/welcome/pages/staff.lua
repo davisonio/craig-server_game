a50f73087b212c323688ca0a53ae7d7a558a77ad
Amaz <Amaz1@users.noreply.github.com>
2015-06-16 16:26:22 +0100

    Add the listring functionality https://github.com/minetest/minetest/commit/c977fbd9288d0e80019554411a2f1885f7760568

--- mods/welcome/pages/staff.lua
+++ mods/welcome/pages/staff.lua
@@ -1,80 +1,18 @@
 function welcome.staff_formspec(player)
-	local name = player:get_player_name()
-	minetest.show_formspec(name, "welcome:staff",
-		welcome.size..
-		welcome.close_button..
-		welcome.image_button_welcome..
-		welcome.image_button_profile..
-		welcome.image_button_rules..
-		welcome.image_button_guides..
-		welcome.image_button_contact..
-		welcome.image_button_inbox..
-		welcome.image_button_staff..
-		"tablecolumns[color;text]" ..
-		"tableoptions[background=#00000000;highlight=#00000000;border=false]" ..
-		"table[2,0;10,2;welcome_staff;" ..
-		"#FFFF00," .. "Welcome to the super-secret staff panel," ..
-		";1]")
-		--"button[2,0.5;2,1;welcome_staff_howto;Howto Guide]"..
-		--"button[4,0.5;2,1;welcome_staff_xban;xban GUI]"
+
 end
 
-minetest.register_on_player_receive_fields(function(player,formname,fields)
-	local plname = player:get_player_name()
-	if formname ~= "welcome:staff" then
-		return
-	end
-	if fields.welcome_welcome then
-		minetest.after(1, function()
-			welcome.welcome_formspec(player)
-		end)
-	end
-	if fields.welcome_profile then
-		minetest.after(1, function()
-			welcome.profile_formspec(player)
-		end)
-	end
-	if fields.welcome_rules then
-		minetest.after(1, function()
-			welcome.rules_formspec(player)
-		end)
-	end
-	if fields.welcome_guides then
-		minetest.after(1, function()
-			welcome.guides_formspec(player)
-		end)
-	end
-	if fields.welcome_contact then
-		minetest.after(1, function()
-			welcome.contact_formspec(player)
-		end)
-	end
-	if fields.welcome_inbox then
-		minetest.after(1, function()
-			chatplus.showInbox(plname)
-		end)
-	end
-	if fields.welcome_staff then
-		minetest.after(1, function()
-			welcome.staff_formspec(player)
-		end)
-	end
-end)
-
-minetest.register_chatcommand("staff",{
+minetest.register_chatcommand("test",{
 	params = "",
 	description = "Shows the welcome page",
 	privs = {shout=true},
 	func = function (name)
-		local player = minetest.get_player_by_name(name)
-		local name = player:get_player_name()
-		minetest.chat_send_player(name, "Showing page... if this doesn't work please try again.")
-		if minetest.get_player_privs(name).rank_sysadmin or minetest.get_player_privs(name).rank_admin or minetest.get_player_privs(name).rank_moderator then
-			minetest.after(1, function()
-				welcome.staff_formspec(player)
-			end)
-		else
-			minetest.chat_send_player(name, "You are not a staff member!")
-		end
+		minetest.after(1, function()
+			local name = player:get_player_name()
+				minetest.show_formspec(name, "test",
+					"size[10,8]"..
+					"test"
+				)
+		end)
 	end,
 })
