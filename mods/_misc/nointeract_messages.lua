2d40afbc65f76504f2edd7d3013ee36926bea29e
Craig Davison <craig.davison3@gmail.com>
2015-06-20 19:54:55 +0100

    Add message for if people have not set their email

--- mods/_misc/nointeract_messages.lua
+++ mods/_misc/nointeract_messages.lua
@@ -1,12 +1,25 @@
 local TIMER = 0
 minetest.register_globalstep(function(dtime)
 	TIMER = TIMER + dtime
-	if TIMER < 30 then return end
+	if TIMER < 60 then return end -- 1 minute
 	TIMER = 0
 	for _,player in ipairs(minetest.get_connected_players()) do
 		local name = player:get_player_name()
 		if not minetest.check_player_privs(name, {interact=true}) then
-			minetest.chat_send_player(name, "Hi " .. name .. "! To build on this server you need to accept the rules. Type /rules")
+			minetest.chat_send_player(name, name .. ", to build on this server you need to accept the rules. Type /rules")
+		end
+	end
+end)
+
+local TIMEFORACTION = 0
+minetest.register_globalstep(function(dtime)
+	TIMEFORACTION = TIMEFORACTION + dtime
+	if TIMEFORACTION < 900 then return end -- 15 minutes 900
+	TIMEFORACTION = 0
+	for _,player in ipairs(minetest.get_connected_players()) do
+		local name = player:get_player_name()
+		if not welcome.emails[name] then
+			minetest.chat_send_player(name, .. name .. ", you have not set your email. Setting your email is important if you forget your password. Type /profile. To stop these messages type /profile nomail.")
 		end
 	end
 end)
