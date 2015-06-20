bf90c8ae4f9686834aa4857cd6aefaf57170e160
Craig Davison <craig.davison3@gmail.com>
2015-06-20 21:05:54 +0100

    Fix message showing when you had set your email

--- mods/_misc/nointeract_messages.lua
+++ mods/_misc/nointeract_messages.lua
@@ -14,12 +14,13 @@ end)
 local TIMEFORACTION = 0
 minetest.register_globalstep(function(dtime)
 	TIMEFORACTION = TIMEFORACTION + dtime
-	if TIMEFORACTION < 900 then return end -- 15 minutes 900
+	if TIMEFORACTION < 900 then return end -- 15 minutes
 	TIMEFORACTION = 0
 	for _,player in ipairs(minetest.get_connected_players()) do
 		local name = player:get_player_name()
-		if not welcome.emails[name] then
-			minetest.chat_send_player(name, name .. ", you have not set your email. Setting your email is important if you forget your password we can reset your password if you forget it. Type /profile. To stop these messages type /profile nomail.")
+		welcome.emails_load()
+		if welcome.emails[name] == nil then
+			minetest.chat_send_player(name, name .. ", you have not set your email. Setting your email is important if you forget your password. Type /profile. To stop these messages type /profile nomail.")
 		end
 	end
 end)
