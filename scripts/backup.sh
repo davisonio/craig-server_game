9cd380fd138aacac926f818f06445afbdf489779
Craig Davison <craig.davison3@gmail.com>
2015-07-28 20:39:50 +0100

    Update backup script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -3,16 +3,14 @@
 # This script is run every day at 9:00 by craig using crontab -e
 # 0 9 * * * /home/craig/minetest/minetest/worlds/server/game/scripts/backup.sh > /dev/null
 
-# todo: use external commands mod to broadcast shutting down message to players
-
-# message shutting down in 30 seconds
-sleep 20
-# message shutting down in 10 seconds
+echo "In 20 seconds, the server will shut-down for daily backups." > ~/minetest/minetest/worlds/server/message
 sleep 5
-# message shutting down in 5 seconds
-sleep 3
-# message shutting down now
-sleep 3
+echo "You will be able to rejoin the world shortly afterwards." > ~/minetest/minetest/worlds/server/message
+sleep 10
+echo "Server shutting down in 5 seconds..." > ~/minetest/minetest/worlds/server/message
+sleep 5
+echo "Server shutting down now!" > ~/minetest/minetest/worlds/server/message
+sleep 2
 
 # First, shutdown the world
 bash ~/minetest/minetest/worlds/server/game/scripts/shutdown.sh
@@ -23,7 +21,7 @@ rsync -azPu ~/minetest/minetest/worlds/server ~/worlds-server.bak
 # Relaunch world after completion
 bash ~/minetest/minetest/worlds/server/game/scripts/launch.sh
 
-# Move world to offserver backup
+# Move world to off-site backup
 DATE="date +%Y-%m-%d"
 TARGET="craig@craig.zzzz.io:~/davisonio-minetest-srv1/$DATE"
 rsync -azPu ~/worlds-server.bak $TARGET
