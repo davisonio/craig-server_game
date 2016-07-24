13c0d670a2ff881c772e45d9268cdc84aed15790
Craig Davison <craig@davison.io>
2016-07-24 12:23:57 +0100

    Update rules
    Use full stops at the end
    interact (build) -> build (interact)

--- mods/interact/config.lua
+++ mods/interact/config.lua
@@ -24,13 +24,13 @@ interact.s3_header = "These are the rules, do you agree to them?"
 --The actual rules.
 interact.s3_rules = [[
 1. Don't attempt to crash, lag, damage, disable or exploit the server or game.
-2. No stealing or greifing (damaging creations of others) 
-3. No harassing (e.g. asking for stuff/privileges constantly)
-4. No PVP (punching others) unless the players taking part agree
-5. Don't use multiple accounts or impersonate another account
-6. No play dating or family role-play. Seriously, not cool
-7. Feel free to walk around anywhere unless the area is clearly private
-8. Please avoid making a mess of the landscape. Nice buildings are always welcome :)
+2. No stealing or greifing (damaging creations of others).
+3. No harassing (e.g. asking for stuff/privileges constantly).
+4. No PVP (punching others) unless the players taking part agree.
+5. Don't use multiple accounts or impersonate another account.
+6. No play dating or family role-play. Seriously, not cool.
+7. Feel free to walk around anywhere unless the area is clearly private.
+8. Please avoid making a mess of the landscape. Nice buildings are always welcome. :)
 
 Failure to follow these rules may result in sanctions.
 ]]
@@ -40,8 +40,8 @@ interact.s3_b1 = "I agree :)"
 interact.s3_b2 = "I disagree :("
 
 --The messages send to the player after interact is granted.
-interact.s3_interact_msg1 = "Thanks for accepting the rules, you now are able to interact (build)."
+interact.s3_interact_msg1 = "Thanks for accepting the rules, you now are able to build (interact)."
 interact.s3_interact_msg2 = "Happy building!"
 
 --The message to send players who disagree when they are kicked for disagring with the rules.
-interact.s3_disagree_msg = "Sorry, you have to agree in order to interact (build). Type /rules if you change your mind."
+interact.s3_disagree_msg = "Sorry, you have to agree in order to build (interact). Type /rules if you change your mind."
