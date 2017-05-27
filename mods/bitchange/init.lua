20c15b5dab9c9fa2988b3136a752ee8d5682f43a
lisacvuk <lisacvukhome@gmail.com>
2017-05-27 21:59:11 +0200

    Remove bitcoin adding (#77)

--- mods/bitchange/init.lua
+++ mods/bitchange/init.lua
@@ -13,20 +13,20 @@ end
 -- Players will get money every day by digging or placing blocks
 players_income = {}
 
-local timer = 0
-minetest.register_globalstep(function(dtime)
-	timer = timer + dtime;
-	if timer >= 720 then -- One day
-		timer = 0
-		for _,player in ipairs(minetest.get_connected_players()) do
-			local name = player:get_player_name()
-			if players_income[name] == nil then
-				players_income[name] = 0
-			end
-			players_income[name] = 3
-		end
-	end
-end)
+--local timer = 0
+--minetest.register_globalstep(function(dtime)
+--	timer = timer + dtime;
+--	if timer >= 720 then -- One day
+--		timer = 0
+--		for _,player in ipairs(minetest.get_connected_players()) do
+--			local name = player:get_player_name()
+--			if players_income[name] == nil then
+--				players_income[name] = 0
+--			end
+--			players_income[name] = 3
+--		end
+--	end
+--end)
 
 earn_income = function(player)
 	if not player then return end
@@ -42,10 +42,10 @@ earn_income = function(player)
 	end
 end
 
-minetest.register_on_dignode(function(pos, oldnode, digger)
-	earn_income(digger)
-end)
+--minetest.register_on_dignode(function(pos, oldnode, digger)
+--	earn_income(digger)
+--end)
 
-minetest.register_on_placenode(function(pos, node, placer)
-	earn_income(placer)
-end)
+--minetest.register_on_placenode(function(pos, node, placer)
+--	earn_income(placer)
+--end)
