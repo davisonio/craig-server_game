6cf938f7356f0a3689a1a4e68a38d10d067d0f28
Craig Davison <craig@davison.io>
2017-05-28 19:31:36 +0100

    Update motd

--- mods/_misc/motd.lua
+++ mods/_misc/motd.lua
@@ -1,4 +1,4 @@
---minetest.register_on_joinplayer(function(player)
---	local message = "# Server:"
---	minetest.chat_send_player(player:get_player_name(), message)
---end)
+minetest.register_on_joinplayer(function(player)
+	local message = "# Server: Skins & Sethome have recently been removed from the inventory. Use the commands /skin, /sethome and /home."
+	minetest.chat_send_player(player:get_player_name(), message)
+end)
