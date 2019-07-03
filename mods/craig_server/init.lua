9dcb271f59f67e42f2394ba4defcbbfa00171546
neinwhal <neinwhal@gmail.com>
2019-07-03 16:11:52 +0800

    Add black and green flowers to v6 decorations

--- mods/craig_server/init.lua
+++ mods/craig_server/init.lua
@@ -25,3 +25,6 @@ dofile(minetest.get_modpath("craig_server").."/spawn.lua")
 
 -- Fix main inventory (incompatible with sinv)
 dofile(minetest.get_modpath("craig_server").."/main_inv_fix.lua")
+
+-- Flowers v6 gen
+dofile(minetest.get_modpath("craig_server").."/gen_flowers.lua")
