6edcb989ebf00b642bdb54dd38917c776edd9a17
neinwhal <neinwhal@gmail.com>
2019-07-03 17:03:59 +0800

    Readd some old default fences

--- mods/craig_server/init.lua
+++ mods/craig_server/init.lua
@@ -25,3 +25,6 @@ dofile(minetest.get_modpath("craig_server").."/spawn.lua")
 
 -- Fix main inventory (incompatible with sinv)
 dofile(minetest.get_modpath("craig_server").."/main_inv_fix.lua")
+
+-- Readd some old fences that were removed in latest git
+dofile(minetest.get_modpath("craig_server").."/old_fences.lua")
