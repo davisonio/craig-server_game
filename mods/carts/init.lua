fed4782c97d0a7ded1d22dd2c0b53ce6406431e8
Craig Davison <craig@davison.io>
2020-12-17 14:17:59 +0000

    Update cart

--- mods/carts/init.lua
+++ mods/carts/init.lua
@@ -1,7 +1,12 @@
+-- carts/init.lua
+
+-- Load support for MT game translation.
+local S = minetest.get_translator("carts")
 
 carts = {}
 carts.modpath = minetest.get_modpath("carts")
 carts.railparams = {}
+carts.get_translator = S
 
 -- Maximal speed of the cart in m/s (min = -1)
 carts.speed_max = 7
@@ -14,3 +19,10 @@ carts.path_distance_max = 3
 dofile(carts.modpath.."/functions.lua")
 dofile(carts.modpath.."/rails.lua")
 dofile(carts.modpath.."/cart_entity.lua")
+
+-- Register rails as dungeon loot
+if minetest.global_exists("dungeon_loot") then
+	dungeon_loot.register({
+		name = "carts:rail", chance = 0.35, count = {1, 6}
+	})
+end
